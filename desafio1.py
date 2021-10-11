53c75101b688de6304d0769507625b3dccf5d267
Stefane Brito <68233371+stefaneSI@users.noreply.github.com>
2021-10-11 11:26:50 +0000

    Criando função no desafio 1 e alterando readme.

--- desafio1.py
+++ desafio1.py
@@ -1,24 +1,18 @@
-while True:
-  try:
-    nota = int(input())
-    
-    if 0 <= nota < 1:
-      print('E')
-      continue
-    elif 1 <= nota <= 35:
-      print('D')
-      continue
-    elif 36 <= nota <= 60:
-      print('C')
-      continue
-    elif 61 <= nota <= 85:
-      print('B')
-      continue
-    elif 86 <= nota <= 100:
-      print('A')
-      continue
-  except ValueError:
-    print("Somente são aceitos números inteiro")
-    break
+def converteNota(note):
+  if 0 <= note < 1:
+    return 'E'
+  elif 1 <= note <= 35:
+    return 'D'
+  elif 36 <= note <= 60:
+    return 'C'
+  elif 61 <= note <= 85:
+    return 'B'
+  elif 86 <= note <= 100:
+    return 'A'
 
-
+try:
+  nota = int(input())
+  print(converteNota(nota))
+except ValueError:
+  print("Somente são aceitos números inteiro")
+  
--- desafio2.py
+++ desafio2.py
@@ -1,5 +1,8 @@
-x = int(input())
-for i in range (1,x+1):
-   #complete
+try:
+  x = int(input())
+  
+  
+  for i in range (1,x+1):
+    #complete
         square = i*i
         print("%i^2 = %i" %(i, square))
