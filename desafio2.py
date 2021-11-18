3215c2cc963cade61b289a210914f23e6b8942ac
Stefane Brito <68233371+stefaneSI@users.noreply.github.com>
2021-11-18 12:19:47 +0000

    Mudança em estrutura de arquivos

--- desafio2.py
+++ desafio2.py
@@ -1,8 +1,17 @@
+def vetor(x):
+  n = 9
+  vet = []
+  for i in range (n):
+    vet[i] = x
+    x = x*2
+        #square = i*i
+    print("%i^2 = %i" %(i, x))
+
 try:
   x = int(input())
+  print(vetor(x))
+except ValueError:
+  print("Somente são aceitos números inteiro")
+
+
   
-  
-  for i in range (1,x+1):
-    #complete
-        square = i*i
-        print("%i^2 = %i" %(i, square))
---
+++ teste.py
@@ -0,0 +1,8 @@
+#import desafio1
+#import desafio2
+
+#desafio = print(input("Selecione o desafio:"))
+#if desafio == 1:
+#  desafio1
+#elif desafio == 2:
+#  desafio2
